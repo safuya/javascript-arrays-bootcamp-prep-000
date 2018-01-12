d7d22e464aa3a867ed13586ac0ca9a14989eb7af
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-12 19:11:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,6 +15,5 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  array.push(element)
-  return array
+  return [...array, element]
 }
